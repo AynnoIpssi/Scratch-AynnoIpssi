ce456011994af2d322f31448dd22a69ede2ab614
Yoann <yt.fayolle@ecole-ipssi.net>
2026-02-11 13:46:53 +0100

    adding drag and drop , block , identity block

--- src/02-main.js
+++ src/02-main.js
@@ -1,88 +1,13 @@
-// changemennt backgroud pour block selon catégories
+import categorieModule from './05-ui/02-panels/01-categoriePanel.js';
+import dragModule from './03-core/02-program.js'
 
-const categorieMoove = document.querySelector("#moove")
-const categorieControl = document.querySelector("#control")
-const categorieEvent = document.querySelector("#event")
-const categorieOperator = document.querySelector("#operator")
-const categorieVariable = document.querySelector("#variable")
-const blockPanel  = document.querySelector(".block-panel")
-
-categorieMoove.addEventListener("click", function(){
-    blockPanel.style.backgroundColor = "#3E55FF";
-})
-
-categorieControl.addEventListener("click", function(){
-    blockPanel.style.backgroundColor = "#FFE53E";
-})
-
-categorieEvent.addEventListener("click", function(){
-    blockPanel.style.backgroundColor = "#A21FFF";
-})
-
-categorieOperator.addEventListener("click", function(){
-    blockPanel.style.backgroundColor = "#1FFF6A";
-})
-
-categorieVariable.addEventListener("click", function(){
-    blockPanel.style.backgroundColor = "#F63131";
-})
-
-// Interface dev (plus de place pour poser les blocks)
-
-const imgExpand = document.querySelector(".expand-arrow")
-const rightPart = document.querySelector(".right-part")
-const leftPart = document.querySelector(".left-part")
-const component = document.querySelector("#component")
-const componentContent = document.querySelector(".component-content")
-const categorie = document.querySelector(".categorie")
-const assemble = document.querySelector("#assemble")
-const preview = document.querySelector("#preview")
-const board = document.querySelector(".board")
-
-
-
-imgExpand.addEventListener("click", function(){
-    rightPart.classList.toggle("active")
-    leftPart.classList.toggle("active")
-    component.classList.toggle("active")
-    componentContent.classList.toggle("active")
-    categorie.classList.toggle("active")
-    blockPanel.classList.toggle("active")
-    assemble.classList.toggle("active")
-    preview.classList.toggle("active")
-    imgExpand.classList.toggle("active")
-    board.classList.toggle("active")
-    zoomOut.classList.toggle("active")
-})
+categorieModule.removeActiveClasses();
 
 
 
 
 
 
-/** 
-const block = document.addEventListenerAll(".block-exemple1")
-const dropZone = document.querySelector("#assemble")
-
-function knowFree(x, y, width, heigt, blocs){
-    for (let bloc of blocs){
-        const rect = bloc.getBoundingClientRect();
-        const dropRect = getBoundingClentRect();
-        const dx = rect.left - dropRect.left
-        const dy = rect.top - dropRect.top
-
-        if (
-            x < dx + rect.width &&
-            x + width > dx &&
-            y < dy + rect.height &&
-            y + height > dy
-        ){
-            return false;
-        }
-    }
-    return true;
-}
-*/
 
 
 
---
+++ src/04-data/01-blocksCatalogue.js
@@ -0,0 +1,40 @@
+const blockMoove = {
+    id: idGestionnary.createId(),
+    type: "action",
+    categorie: "mouvement",
+    content: "step 10 mooves",
+    children: [],
+};
+
+const blockTurnLeft = {
+    id: idGestionnary.createId(),
+    type: "action",
+    categorie: "mouvement",
+    content: "turn °degree",
+    children: [],
+};
+
+const blockTurnRight = {
+    id: idGestionnary.createId(),
+    type: "action",
+    categorie: "mouvement",
+    content: "turn °degree",
+    children: [],
+};
+
+const blockPosition = {
+    id: idGestionnary.createId(),
+    type: "action",
+    categorie: "mouvement",
+    content: "block position",
+    children: [],
+}
+
+
+const blockIf = {
+    id: idGestionnary.createId(),
+    type: "control",
+    categorie: "controle",
+    content: "if",
+    children: [],
+}
